47c564f1ece6337960d431c325b67884deb0af0e
RodrigoLuigi <digaor@gmail.com>
2023-04-04 15:18:42 -0300

    style: adjust footer style

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,15 @@
 import logoFooter from '../../assets/logo-footer.svg'
 
-import { Container, Logo } from './styles'
+import { Container } from './styles'
 
 export function Footer() {
 	return (
 		<Container>
 			<div className="content">
-				<Logo>
-					<img src={logoFooter} alt="" />
-				</Logo>
+				<img
+					src={logoFooter}
+					alt="Hexagono de cor azul com texto Food Explorer do lado direito"
+				/>
 				<span>&copy;2023 - Todos os direitos reservados.</span>
 			</div>
 		</Container>
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -9,25 +9,20 @@ export const Container = styled.footer`
 	> .content {
 		max-width: 1160px;
 		height: 7.7rem;
+
 		margin: 0 auto;
+		padding: 0 2.3rem;
 
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
 
-		padding: 0 2.3rem;
-	}
-`
+		img {
+			width: clamp(16rem, 10rem + 10vw, 18.6rem);
+		}
 
-export const Logo = styled.div`
-	display: flex;
-	align-items: center;
-
-	img {
-		width: clamp(16rem, 10rem + 10vw, 19rem);
-	}
-
-	span {
-		color: ${({ theme }) => theme.COLORS.LIGHT_200};
+		span {
+			color: ${({ theme }) => theme.COLORS.LIGHT_200};
+		}
 	}
 `
