651c2b723fcf5b5e061a37498ecc1ba9aa6ce991
RodrigoLuigi <digaor@gmail.com>
2023-10-25 22:39:14 -0300

    Update baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-	/* baseURL: 'https://food-explorer-app.onrender.com' */
-	baseURL: 'http://localhost:3333'
+	baseURL: 'https://food-explorer-app.onrender.com'
+	/* baseURL: 'http://localhost:3333' */
 })
 
 // Adiciona o Bearer token no cabeçalho das requisições
